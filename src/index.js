09683170a0438cf46ec21e8ee2578e9df00d9ad3
natacha <nat_bartu@hotmail.com>
2021-11-28 10:33:58 +1100

    change port for deploy test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 // Separate these out in case we wanna use Docker or something to wrap the app.
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 
 // Best settings for setting up Express as an API server to receive and process JSON & form data.
