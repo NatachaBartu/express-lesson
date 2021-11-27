8782be43898b9501e16dda9f2cf849dcf028fc89
natacha <nat_bartu@hotmail.com>
2021-11-27 15:19:19 +1100

    add PORT and HOST

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ const express = require("express");
 //Initialize Express as an instance named "app".
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
-});
-// // Separate these out in case we wanna use Docker or something to wrap the app.
-// const PORT = 3000;
-// const HOST = "0.0.0.0";
+const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
 
-// // Best settings for setting up Express as an API server to receive and process JSON & form data.
-app.listen(3000, () => {
+app.use(express.urlencoded({ extended: true }));
+
+// We can receive JSON data from POST/ PUT/ PATCH/ etc requests
+app.get("/", (request, response) => {
+  response.json();
+  //   Same as above but for form data
+});
+
+app.listen(PORT, HOST, () => {
   console.log("Server is running");
 });
